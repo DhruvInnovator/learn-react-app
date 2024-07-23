e6fc36e874597fc7b1faeec2785bb9b711733a87
DhRuVUser04 <dhruvnegi2040@gmail.com>
2024-07-23 19:43:47 +0530

    Starting a New project

---
+++ ReactRoute_07/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from './components/Header'
+import './App.css'
+import Footer from './components/Footer'
+import Home from './components/Home/Home'
+
+function App() {
+
+  return (
+  <div>
+      <Header />
+      <Footer />
+      <Home />
+  </div>
+  )
+}
+
+export default App
+  
---
+++ ReactRoute_07/src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './Home/Home'
+import About from './About'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>,
+    children: [
+      {
+        path  : '/',
+        element: <Home/>
+      },
+      {
+        path  : '/about',
+        element: <About/>
+      },
+      {
+        // path  : '/services',
+        // element: <Services/>
+      },
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
